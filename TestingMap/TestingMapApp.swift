b6e9dd33b17bd3bf9862706678a39ba091d0847a
ZinThantWin <zinthantwin1997@gmail.com>
2024-06-25 13:01:54 +0630

    Initial Commit

---
+++ TestingMap/TestingMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingMapApp.swift
+//  TestingMap
+//
+//  Created by ကင်ဇို on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestingMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
